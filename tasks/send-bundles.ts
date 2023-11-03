9f4f2412db73629f00bae681f3b54150940ee9d3
halo3mic <mihalo3c@gmail.com>
2023-11-03 02:02:06 +0000

    submit & build

--- tasks/send-bundles.ts
+++ tasks/send-bundles.ts
@@ -2,19 +2,17 @@ import { HardhatRuntimeEnvironment as HRE } from 'hardhat/types';
 import { task, types } from 'hardhat/config';
 import { ethers, Wallet } from 'ethers';
 
-import { SUAVE_CHAIN_ID } from '../src/const';
+import { SUAVE_CHAIN_ID, PRECOMPILES } from '../src/const';
 import { 
 	ConfidentialComputeRequest, 
 	ConfidentialComputeRecord 
 } from '../src/confidential-types'
 import * as utils from './utils';
 
-
-const precompiles = { // todo: move
-	buildEthBlock: '0x0000000000000000000000000000000042100001'
-}
-
+export type Result<T> = [T, null] | [null, string]
 const abis = utils.fetchAbis()
+const mevshareInterface = new ethers.utils.Interface(abis['MevShareBidContract'])
+
 
 task('send-bundles', 'Send Mevshare Bundles for the next N blocks')
 	.addOptionalParam("nblocks", "Number of blocks to send bundles for. Default is two.", 1, types.int)
@@ -33,39 +31,61 @@ task('send-bundles', 'Send Mevshare Bundles for the next N blocks')
 	
 
 async function sendMevShareBidTxs(c: ITaskConfig) {
-	const mevshareInterface = new ethers.utils.Interface(abis['MevShareBidContract'])
-	const confidentialDataBytes = await makeDummyBundleBytes(c.goerliSigner);
-	const allowedPeekers = [c.mevshareAdd, c.builderAdd, precompiles.buildEthBlock];
+	const confidentialDataBytes = await makeDummyTx(c.goerliSigner).then(makeDummyBundleBytes);
+	const allowedPeekers = [c.mevshareAdd, c.builderAdd, PRECOMPILES.buildEthBlock];
 	const allowedStores = [];
 	
 	let startGoerliBlock = await c.goerliSigner.provider.getBlockNumber();
 	console.log('startingGoerliBlockNum', startGoerliBlock);
 	for (let blockNum = startGoerliBlock + 1; blockNum < startGoerliBlock + c.nBlocks + 1; blockNum++) {
-		const calldata = mevshareInterface
-			.encodeFunctionData('newBid', [blockNum, allowedPeekers, allowedStores])
-		const mevShareConfidentialRec = await prepareMevShareBidTx(
+		let [success, err] = await sendBidForBlock(
 			c.suaveSigner, 
-			calldata, 
 			c.executionNodeAdd, 
-			c.mevshareAdd
-		);
-		
-		const inputBytes = new ConfidentialComputeRequest(mevShareConfidentialRec, confidentialDataBytes)
-			.signWithWallet(c.suaveSigner)
-			.rlpEncode()
-
-		const response = await (c.suaveSigner.provider as any).send('eth_sendRawTransaction', [inputBytes])
-			.catch(err => {
-				console.log('err submitting tx', err)
-			})
-		console.log(`tx: ${response}`)
-		await handleNewSubmission(c.suaveSigner.provider, mevshareInterface, response)
+			c.mevshareAdd, 
+			confidentialDataBytes, 
+			blockNum, 
+			allowedPeekers, 
+			allowedStores
+		)
+		if (err) {
+			console.log(err)
+		} else {
+			await success.then(console.log)
+		}
 	}
 
 }
 
-async function makeDummyBundleBytes(signer): Promise<string> {
-	const signedTx = await makeDummyTx(signer);
+export async function sendBidForBlock(
+	suaveSigner: Wallet, 
+	executionNodeAdd: string,
+	mevshareAdd: string,
+	confidentialDataBytes: string,
+	blockNum: number, 
+	allowedPeekers: string[], 
+	allowedStores: string[],
+): Promise<Result<Promise<string>>> {
+	const calldata = mevshareInterface
+			.encodeFunctionData('newBid', [blockNum, allowedPeekers, allowedStores])
+	const mevShareConfidentialRec = await prepareMevShareBidTx(
+		suaveSigner, 
+		calldata, 
+		executionNodeAdd, 
+		mevshareAdd
+	);
+	
+	const inputBytes = new ConfidentialComputeRequest(mevShareConfidentialRec, confidentialDataBytes)
+		.signWithWallet(suaveSigner)
+		.rlpEncode()
+
+	const result = await (suaveSigner.provider as any).send('eth_sendRawTransaction', [inputBytes])
+		.then(r => [handleNewSubmission(suaveSigner.provider, r), null])
+		.catch(err => [null, handleErr(err)])
+
+	return result
+}
+
+export function makeDummyBundleBytes(signedTx): string {
 	const bundle = utils.txToBundle(signedTx);
 	const bundleBytes = utils.makeConfidentialDataBytesFromBundle(bundle);
 	return bundleBytes;
@@ -105,29 +125,51 @@ async function makeDummyTx(signer) {
 	return signed;
 }
 
-async function handleNewSubmission(provider, mevshareInterface, txHash) {
-	const receipt = await provider.waitForTransaction(txHash)
+async function handleNewSubmission(provider, txHash): Promise<string> {
+	const mevshareInterface = new ethers.utils.Interface(abis['MevShareBidContract'])
+	const receipt = await provider.waitForTransaction(txHash, 1)
+
+	let output = `\tBid tx ${txHash} confirmed:`
 	if (receipt.status === 0) {
-		console.log(`⛔️ tx failed`)
-		console.log(`${JSON.stringify(receipt)}`)
+		output += `\t❌ Bid submission failed`
+		output += `\n\t${JSON.stringify(receipt)}`
 	} else {
-		const tab = n => '  '.repeat(n)
-		console.log(`\n✅ tx succeeded`)
+		const tab = n => '\t  '.repeat(n)
+		output += `\n\t✅ Bid submission succeeded\n`
 		receipt.logs.forEach(log => {
 			const parsedLog = mevshareInterface.parseLog(log);
-			console.log(`${tab(1)}${parsedLog.name}`)
+			output += `${tab(1)}${parsedLog.name}\n`
 			parsedLog.eventFragment.inputs.forEach((input, i) => {
 				if (parsedLog.name == "HintEvent" && input.name == "hint") {
 					const hintTo = parsedLog.args[i].slice(0, 43)
 					const hintData = '0x' + parsedLog.args[i].slice(43)
-					console.log(`${tab(2)}${input.name}:\n${tab(3)}to: ${hintTo}\n${tab(3)}data: ${hintData}`)
+					output += `${tab(2)}${input.name}:\n${tab(3)}to: ${hintTo}\n${tab(3)}data: ${hintData}\n`
 				} else {
-					console.log(`${tab(2)}${input.name}: ${parsedLog.args[i]}`)
+					output += `${tab(2)}${input.name}: ${parsedLog.args[i]}\n`
 				}
 			})
 		})
 	}
-} 
+
+	return output + '\n'
+}
+
+function handleErr(err): string {
+	const rpcErr = JSON.parse(err.body)?.error?.message
+	if (rpcErr && rpcErr.startsWith('execution reverted: ')) {
+		const revertMsg = rpcErr.slice('execution reverted: '.length)
+		const decodedErr = new ethers.utils.Interface(abis['EthBlockBidSenderContract'])
+			.decodeErrorResult(revertMsg.slice(0, 10), revertMsg)
+		if (revertMsg.startsWith('0x75fff467')) {
+			const errStr = Buffer.from(decodedErr[1].slice(2), 'hex').toString()
+			return `❗️ PeekerReverted(${decodedErr[0]}, '${errStr})'`
+		} else {
+			return `❗️ ` + rpcErr + '\n Params: ' + decodedErr.join(',')
+		}
+	} else {
+		return `❗️ ` + rpcErr
+	}
+}
 
 interface ITaskConfig {
 	nBlocks: number,
